89e102eae952d9c5c47fc2182da5f9f4fa33e4bf
darleg <darleg@outlook.com>
2022-02-27 09:51:06 -0800

    Update BifurcationPlot.py

--- Python/Bifur/BifurcationPlot.py
+++ Python/Bifur/BifurcationPlot.py
@@ -6,8 +6,8 @@ from numpy import empty, linspace, random
 
 
 # x and y data containers
-xdc = []
-ydc = []
+xdc = [] # for r control parameters
+ydc = [] # for x values of the Logistic map
 
 # r range to plot
 rmin = 2.9
@@ -15,10 +15,10 @@ rmax = 4
 # number of samples to generate
 samples = 5000
 x=0.7
-
+# setup control prameter range
 R = linspace(rmin,rmax,samples)
 
-
+# Generate x for each value or r
 for r in R:
     # Collecting r componet into xdc data container
     xdc.append(r)
